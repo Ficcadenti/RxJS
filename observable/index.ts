b499fe94cfc60e89355a308d1ebd996cbc7cecb1
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-02-04 16:09:24 +0100

    Observable

--- observable/index.ts
+++ observable/index.ts
@@ -3,29 +3,17 @@ import { Observable } from './node_modules/rxjs/';
 const objs = new Observable(subscriber => {
 
     // next, error, 
-    // 1---2---3
+    // 1---2---3---4---5
     subscriber.next(1);
     subscriber.next(2);
-    setTimeout(() => { subscriber.next(4); }, 4000);
-    //subscriber.complete();
-    subscriber.next(3);// it won't be delivered
-
+    subscriber.next(3);
+    subscriber.next(4);
+    subscriber.complete();
+    subscriber.next(5);// non viene emesso
 });
-objs.subscribe(
-    v => {
-        console.log(v);
-    }
+objs.subscribe(x => { console.log(x) }
     , error => {
         console.log(error)
     }, () => {
-        console.log('values completed');
-    }
-);
-console.log('subscribe 1 end');
-objs.subscribe({
-    next: v => {
-        console.log('subscribe2 =' + v)
-    },
-    complete: () => { console.log('Second subscribe finished') },
-    error: error => console.log(error)
-});
+        console.log('Valori terminati !!!!');
+    });
