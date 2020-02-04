279d1c08723480b94abc38ec044375c78039f784
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-02-04 16:27:22 +0100

    Observable

--- Documentazione/readme.txt
+++ Documentazione/readme.txt
@@ -3,6 +3,7 @@ https://rxjs-dev.firebaseapp.com/
 http://reactivex.io/
 https://rxmarbles.com/
 https://rxjs-dev.firebaseapp.com/guide/observable
+https://www.freewebsolution.it/cosa-sono-gli-observable-e-come-utilizzarli-in-angular/
 
 #############################################################################################################################################
 #############################################################################################################################################
@@ -10,3 +11,10 @@ https://rxjs-dev.firebaseapp.com/guide/observable
 
 ######### Installazione
 npm install rxjs
+
+######### Creare un nuovo prj
+npm i --save-dev typescript
+npm init -y 
+tsc --init
+npm install rxjs
+npm install @types/node --save-dev
--- observable/index.ts
+++ observable/index.ts
@@ -7,13 +7,20 @@ const objs = new Observable(subscriber => {
     subscriber.next(1);
     subscriber.next(2);
     subscriber.next(3);
-    subscriber.next(4);
-    subscriber.complete();
+    setTimeout(() => { subscriber.next(4); }, 4000); //async
+    //subscriber.complete(); //sync
     subscriber.next(5);// non viene emesso
 });
-objs.subscribe(x => { console.log(x) }
-    , error => {
-        console.log(error)
-    }, () => {
-        console.log('Valori terminati !!!!');
-    });
+objs.subscribe(x => {
+    console.log('subscribe1 = ' + x)
+}, error => {
+    console.log(error)
+}, () => {
+    console.log('Valori terminati !!!!');
+});
+
+objs.subscribe({
+    next: v => { console.log('subscribe2 = ' + v) },
+    complete: () => { console.log('Second subscribe finished') },
+    error: error => console.log(error)
+});
