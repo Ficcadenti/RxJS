b7e9658977bfe44ecbb5ba63526c14c2a994204f
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-02-04 15:41:21 +0100

    Primo esempio RxJS

--- esempio1/index.ts
+++ esempio1/index.ts
@@ -1,1 +1,8 @@
-console.log("Esempio 1 - ReactiveX JS!!!!");
+import { range } from './node_modules/rxjs/';
+import { map, filter } from './node_modules/rxjs/operators';
+
+//-1-2-3-4-5-6-
+range(1, 200).pipe(
+    filter(x => x % 2 === 0),
+    map(x => x * x)
+).subscribe(x => console.log(x));
