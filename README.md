6ac58277206777d353092c6abfd807028e13ca75
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-02-12 11:29:01 +0100

    Google book APIs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # RxJS
 Reactive Extensions Library per JavaScript - Esempi pratici
+
+
+RxJS
+<a href="https://rxjs-dev.firebaseapp.com/">https://rxjs-dev.firebaseapp.com</a>
+<a href="http://reactivex.io/">http://reactivex.io</a>
+<a href="https://rxmarbles.com/">https://rxmarbles.com</a>
+<a href="https://rxjs-dev.firebaseapp.com/guide/observable">https://rxjs-dev.firebaseapp.com/guide/observable</a>
+<a href="https://www.freewebsolution.it/cosa-sono-gli-observable-e-come-utilizzarli-in-angular/">https://www.freewebsolution.it/cosa-sono-gli-observable-e-come-utilizzarli-in-angular</a>
+
+Node Fetch
+<a href="https://www.npmjs.com/package/node-fetch">https://www.npmjs.com/package/node-fetch</a>
+
+JSONPlaceholder
+<a href="https://jsonplaceholder.typicode.com/">https://jsonplaceholder.typicode.com/</a>
+
+http-server
+<a href="https://www.npmjs.com/package/http-server">https://www.npmjs.com/package/http-server</a>
+
+Google-Books-APIs
+<a href="https://developers.google.com/books">https://developers.google.com/books</a>
+
+Bootstrap
+<a href="https://getbootstrap.com/">https://getbootstrap.com/</a>
+
+Live-server
+<a href="https://www.npmjs.com/package/live-server">https://www.npmjs.com/package/live-server</a>
--- googlebooks/ts/index.ts
+++ googlebooks/ts/index.ts
@@ -1,10 +1,137 @@
-function getBooks(booktitle: string) {
-    const apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
+declare const rxjs: any;
 
-    fetch(apiurl + booktitle).then(res => res.json()).then(
-        books => console.log(books)
-    );
+interface GoogleBooks {
+    kind: string;
+    totalItems: number;
+    items: [];
+}
+interface BookThumbnails {
+    smallThumbnail: string;
+    thumbnail: string;
+}
+interface VolumeInfo {
+    title: string;
+    authors: [];
+    description: string;
+    imageLinks: BookThumbnails;
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    categories: [];
+}
+interface Book {
+    title: string
+    description: string
+    authors: []
+    categories: []
+    thumbnail: string
+}
+interface BookItem {
+    volumeInfo: VolumeInfo
+    id: string
+}
+
+function getDescription(desc: string) {
+    if (typeof desc === "undefined")
+        return '';
+    else
+        return desc.substring(0, 20);
+}
+
+
+function displayBook(book: Book) {
+
+
+    const bookTpl = ` <div class="card mb-4 shadow-sm">
+                            <img src="${book.thumbnail}" title="${book.title}" alt="${book.title}">
+                            <div class="card-body">
+                            <h4>${book.title}</h4>
+                                <p class="card-text">${getDescription(book.description)}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    const div = document.createElement('div');
+    const books = document.querySelector('#books');;
+    div.setAttribute('class', 'col-md-3');
+    div.innerHTML = bookTpl;
+    if (books) {
+        books.appendChild(div);
+    }
 
 }
 
-getBooks('game of thrones');
+function showTotal(total: number) {
+    const found = document.querySelector('#found');
+    if (found) {
+        found.textContent = '' + total;
+    }
+}
+
+function getBooks(booktitle: string) {
+    const { from } = rxjs;
+    const { map, switchMap, tap } = rxjs.operators;
+    const apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+    const p = fetch(apiurl + booktitle).then(res => res.json());
+
+    return from(p).pipe(
+        tap((data: GoogleBooks) => showTotal(data.totalItems)),
+        switchMap((data: GoogleBooks) => from(data.items || [])),
+        map((ele: BookItem) => {
+            const book: Book = {
+                title: ele.volumeInfo.title,
+                categories: ele.volumeInfo.categories,
+                authors: ele.volumeInfo.authors,
+                description: ele.volumeInfo.description,
+                thumbnail: ele.volumeInfo.imageLinks ? ele.volumeInfo.imageLinks.thumbnail : ''
+            };
+            return book;
+        }
+        )
+    )
+
+}
+
+function cleanBooks() {
+    const books = document.querySelector('#books');
+    if (books) {
+        books.innerHTML = '';
+    }
+}
+
+function searckBooks() {
+    const searchEle = document.querySelector('#search');
+    const { fromEvent } = rxjs;
+    const { filter, map, switchMap, debounceTime, tap } = rxjs.operators;
+    if (searchEle) {
+        fromEvent(searchEle, 'keyup')
+            .pipe(
+                map((ele: any) => ele.target.value),
+                filter((ele: string) => ele.length > 2),
+                debounceTime(1200),
+                tap(() => cleanBooks()),
+                switchMap((ele: string) => getBooks(ele))
+            )
+
+            .subscribe((book: Book) => displayBook(book));
+
+    }
+
+}
+
+function searchButtonClicked() {
+    const books: any = document.querySelector('#search');
+    if (books) {
+        getBooks(books.value)
+            .subscribe((book: Book) => displayBook(book))
+    }
+}
+
+searckBooks();
